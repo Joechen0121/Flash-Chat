31ae3d60cc6016dc895fc1d392cb13580500ef94
Joechen <s900567@yahoo.com.tw>
2022-04-02 15:38:49 +0800

    [Add] Flash-Chat using Firebase

---
+++ Flash Chat iOS13/Constant.swift
@@ -0,0 +1,30 @@
+//
+//  Constant.swift
+//  Flash Chat iOS13
+//
+//  Created by 陳建綸 on 2022/3/28.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue: String = "RegisterToChat"
+    static let loginSegue: String = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lightBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+    
+}
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 陳建綸 on 2022/3/28.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
